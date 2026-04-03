ad37f84f698841b238be5206893ef7962899041a
Joshua Goetz <joshua.l.goetz@outlook.com>
2026-04-02 23:54:17 -0400

    added htmx middleware

--- met_290/settings.py
+++ met_290/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware', # Added
 ]
 
 ROOT_URLCONF = 'met_290.urls'
